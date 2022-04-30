4054d128e6d009f6c6091294a083d1aa659fdafe
starikov03 <you@example.com>
2022-04-30 17:21:07 +0300

    chat 2.0

--- view.mjs
+++ view.mjs
@@ -3,6 +3,8 @@ export const UI_ELEMENTS = {
 	BTN_EXIT: document.querySelector('.dialog__btn-exit'),
 	BTN_CLOSE_SETTINGS: document.querySelector('.settings__close'),
 	SETTINGS_BLOCK: document.querySelector('.settings'),
+	INPUT_SETTINGS: document.querySelector('.settings__input'),
+	BTN_SETTINGS_SUBMIT: document.querySelector('.settings__submit'),
 	DIALOG_BLOCK: document.querySelector('.dialog'),
 	BTN_DIALOD: document.querySelector('.dialog__bottom'),
 	INPUT_DIALOG_MESSAGE: document.querySelector('.dialog__message-input'),
@@ -12,9 +14,16 @@ export const UI_ELEMENTS = {
 	BTN_CLOSE_AUTHORIZATION: document.querySelector('.authorization__close'),
 	AUTHORIZATION_BLOCK: document.querySelector('.authorization'),
 	CONFIRMATION_BLOCK: document.querySelector('.confirmation'),
+	INPUT_CONFIRMATION: document.querySelector('.confirmation__input'),
+	BTN_CONFIRMATION: document.querySelector('.confirmation__submit'),
 	BTN_CLOSE_CONFIRMATION: document.querySelector('.confirmation__close'),
+	BTN_AUTHORIZATION_ENTER : document.querySelector('.Enter'),
+	NITIFICATION : document.querySelector('.Notification'),
 }
 
 export const API_DATA = {
 AUTHORIZATION_CODE_LINK : 'https://mighty-cove-31255.herokuapp.com/api/user',
+USER_LINK : 'https://mighty-cove-31255.herokuapp.com/api/user/me',
+MESSAGES_LINK : 'https://mighty-cove-31255.herokuapp.com/api/messages',
+
 }
